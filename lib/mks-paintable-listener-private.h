5e752163189ecea349bc891aac042a259bdd8b65
Christian Hergert <chergert@redhat.com>
2023-02-09 17:30:38 -0800

    lib: stub internal listener skeleton
    This is an org.qemu.Listener using the gdbus-codegen generated skeleton as
    the parent type.
    
    What will probably happen here, is that we create a MksPaintable which is
    given to the listener to call internal API upon.
    
    The paintable code will be a bit more complex than originally anticipated
    in that we are going to need to do our own tessellation so that we get
    damage regions which are not the whole widget. Very similar to what I did
    in the GdkMacosTile for gdk/macos/.

---
+++ lib/mks-paintable-listener-private.h
@@ -0,0 +1,35 @@
+/*
+ * mks-paintable-listener-private.h
+ *
+ * Copyright 2023 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "mks-qemu.h"
+
+G_BEGIN_DECLS
+
+#define MKS_TYPE_PAINTABLE_LISTENER (mks_paintable_listener_get_type())
+#define MKS_PAINTABLE_LISTENER_OBJECT_PATH "/org/qemu/Display1/Listener"
+
+G_DECLARE_FINAL_TYPE (MksPaintableListener, mks_paintable_listener, MKS, PAINTABLE_LISTENER, MksQemuListenerSkeleton)
+
+MksPaintableListener *mks_paintable_listener_new (void);
+
+G_END_DECLS
